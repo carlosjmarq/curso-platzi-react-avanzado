9d479c60aa03b9648eaee1910061ac42f66f6d83
carlosjmarq <carlosjmarq@gmail.com>
2023-03-24 10:39:04 -0400

    conf: changing for vite - typescript project

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import * as ReactDOM from 'react-dom/client';
-
-var container = ReactDOM.createRoot(document.getElementById('app'))
-container.render(<h1>Hola Mundo!</h1>)
