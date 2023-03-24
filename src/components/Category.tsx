a8250ccb60ad5e2171110b9ddbf9e111b96e0031
carlosjmarq <carlosjmarq@gmail.com>
2023-03-24 12:13:30 -0400

    feat: adding category component

---
+++ src/components/Category.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Anchor, Image } from './styles';
+
+const DEFAULT_IMAGE = 'https://cdn2.thecatapi.com/images/MTc4NzYzMg.jpg';
+
+interface CategoryProps {
+  path: string;
+  emoji: string;
+  cover?: string;
+}
+
+export const Category = ({ cover = DEFAULT_IMAGE, path, emoji }: CategoryProps): JSX.Element => {
+  return (
+    <Anchor href={path}>
+      <Image src={cover} />
+      {emoji}
+    </Anchor>
+  );
+};
---
+++ src/components/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Anchor = styled.a`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  text-decoration: none;
+  width: 5rem;
+`;
+
+export const Image = styled.img`
+  border: 1px solid #ddd;
+  box-shadow: 0px 1rem 2rem rgba(0, 0, 0, 0.2);
+  border-radius: 50%;
+  height: 5rem;
+  width: 5rem;
+  overflow: hidden;
+  object-fit: cover;
+`;
