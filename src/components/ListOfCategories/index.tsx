baba5e56b3cf3a11008a58fa7c60fa43ec2dc6fb
carlosjmarq <carlosjmarq@gmail.com>
2023-04-18 20:01:13 -0400

    feat: adding floating categories

--- src/components/ListOfCategories/index.tsx
+++ src/components/ListOfCategories/index.tsx
@@ -2,19 +2,43 @@ import { Category } from '../Category';
 import { List, ListItem } from './styles';
 import axios from 'axios';
 import { CategoryType } from '../../types/Entities';
+import { useEffect, useState } from 'react';
 
 interface ListCategoriesProps {
   categories: CategoryType[];
 }
 
 export const ListOfCategories = ({ categories, ...props }: ListCategoriesProps): JSX.Element => {
-  return (
-    <List>
+  const [showFixed, setShowFixed] = useState<Boolean>(false);
+
+  useEffect(() => {
+    const onScroll = (event: Event): void => {
+      const newShowFixed = window.scrollY > 200;
+      showFixed !== newShowFixed && setShowFixed(newShowFixed);
+    };
+
+    window.addEventListener('scroll', onScroll);
+    console.log('hfdsajkfhak');
+
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [showFixed]);
+
+  const renderCategoryList = (fixed: Boolean = false): JSX.Element => (
+    <List className={fixed ? 'fixed' : ''}>
       {categories.map((category) => (
         <ListItem key={category.id}>
           <Category {...category} />
         </ListItem>
       ))}
     </List>
+  );
+
+  return (
+    <>
+      {renderCategoryList()}
+      {showFixed && renderCategoryList(showFixed)}
+    </>
   );
 };
